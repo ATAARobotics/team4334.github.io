25c6a5378d1cfc24721d81ea6e27f2994a1016c6
John Brandt <johnb0@outlook.com>
2017-10-24 20:33:30 -0600

    Various fixes and additions:
    -Fixed navbar overlap issue
    -Fixed box length issues on robots, mentors, and alumni pages on smaller
    screens
    -Finished all blog pages and posts
    -Added blog images
    -Finished news page
    -Added photos page
    -Added videos page
    -Added chairmans page
    -Added Beakerhead images
    -Added lightgallery files
    -Moved bootstrap and jquery files to be locally hosted
    -Cleaned up navbar
    -Added sponsors page
    -Added contact page
    -Link blog posts to home page

---
+++ resources/js/lg-photos.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function() {
+  $('#beakerhead2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/Beakerhead2017/Photos/ORG_DSC01953.JPG',
+              thumb: '../albums/Beakerhead2017/Photos/ORG_DSC01953.JPG'
+          },{
+              src: '../albums/Beakerhead2017/Photos/ORG_DSC01980.JPG',
+              thumb: '../albums/Beakerhead2017/Photos/ORG_DSC01980.JPG',
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#houston2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
