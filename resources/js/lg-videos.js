d8378974c6c152bf3141d9515819d0b21928714d
John Brandt <johnb0@outlook.com>
2017-12-18 19:31:04 -0700

    Fix spelling and changes to videos page

---
+++ resources/js/lg-videos.js
@@ -0,0 +1,355 @@
+﻿$(document).ready(function() {
+  $('#beakerhead2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/Beakerhead2017/Photos/1.JPG',
+              thumb: '../albums/Beakerhead2017/Photos/thumbs/1.JPG'
+          },{
+              src: '../albums/Beakerhead2017/Photos/2.JPG',
+              thumb: '../albums/Beakerhead2017/Photos/thumbs/2.JPG',
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#houston2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#westerncanada2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#utah2017').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#beakerhead2016').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#championships2016').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#westerncanada2016').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#utah2016').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: 'http://via.placeholder.com/350x150',
+              thumb: 'http://via.placeholder.com/350x150'
+          },{
+              src: 'https://www.youtube.com/watch?v=meBbDqAXago',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg'
+          },{
+              html: '#video2',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/videos/y-video1-cover.jpg',
+              poster: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-v-y-1.jpg'
+          },{
+              src: 'https://sachinchoolur.github.io/lightGallery/static/img/4.jpg',
+              thumb: 'https://sachinchoolur.github.io/lightGallery/static/img/thumb-4.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#championship2015').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/Championship2015/Photos/1.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/1.png'
+          },{
+              src: '../albums/Championship2015/Photos/2.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/2.png',
+          },{
+              src: '../albums/Championship2015/Photos/3.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/3.png'
+          },{
+              src: '../albums/Championship2015/Photos/4.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/4.png'
+          },{
+              src: '../albums/Championship2015/Photos/5.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/5.png'
+          },{
+              src: '../albums/Championship2015/Photos/6.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/6.png'
+          },{
+              src: '../albums/Championship2015/Photos/7.png',
+              thumb: '../albums/Championship2015/Photos/thumbs/7.png'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#utah2015').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/Utah15/Photos/1.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/1.jpg'
+          },{
+              src: '../albums/Utah15/Photos/2.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/2.jpg',
+          },{
+              src: '../albums/Utah15/Photos/3.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/3.jpg'
+          },{
+              src: '../albums/Utah15/Photos/4.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/4.jpg'
+          },{
+              src: '../albums/Utah15/Photos/5.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/5.jpg'
+          },{
+              src: '../albums/Utah15/Photos/6.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/6.jpg'
+          },{
+              src: '../albums/Utah15/Photos/7.jpg',
+              thumb: '../albums/Utah15/Photos/thumbs/7.jpg'
+          }]
+      });
+  });
+});
+
+$(document).ready(function() {
+  $('#buildseason2015').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/2015BagAndTag/Photos/1.jpg',
+              thumb: '../albums/2015BagAndTag/Photos/thumbs/1.jpg'
+          },{
+              src: '../albums/2015BagAndTag/Photos/2.jpg',
+              thumb: '../albums/2015BagAndTag/Photos/thumbs/2.jpg',
+          },{
+              src: '../albums/2015BagAndTag/Photos/3.jpg',
+              thumb: '../albums/2015BagAndTag/Photos/thumbs/3.jpg'
+          },{
+              src: '../albums/2015BagAndTag/Photos/4.jpg',
+              thumb: '../albums/2015BagAndTag/Photos/thumbs/4.jpg'
+          },{
+          }]
+      });
+  });
+});
+
+
+$(document).ready(function() {
+  $('#westerncanada2014').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/CalgaryRegional2014/Photos/68.jpg',
+              thumb: '../albums/CalgaryRegional2014/Photos/thumbs/68.jpg'
+          },{
+              src: '../albums/CalgaryRegional2014/Photos/69.jpg',
+              thumb: '../albums/CalgaryRegional2014/Photos/thumbs/69.jpg'
+          },{
+              src: '../albums/CalgaryRegional2014/Photos/70.jpg',
+              thumb: '../albums/CalgaryRegional2014/Photos/thumbs/70.jpg'
+          }]
+      });
+  });
+});
+
+
+
+
+$(document).ready(function() {
+  $('#iri2013').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/IRI/Photos/1.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/1.jpg'
+          },{
+              src: '../albums/IRI/Photos/2.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/2.jpg',
+          },{
+              src: '../albums/IRI/Photos/3.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/3.jpg'
+          },{
+              src: '../albums/IRI/Photos/4.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/4.jpg'
+          },{
+              src: '../albums/IRI/Photos/5.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/5.jpg'
+          },{
+              src: '../albums/IRI/Photos/6.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/6.jpg'
+          },{
+              src: '../albums/IRI/Photos/7.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/7.jpg'
+          },{
+              src: '../albums/IRI/Photos/8.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/8.jpg'
+          },{
+              src: '../albums/IRI/Photos/9.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/9.jpg'
+          },{
+              src: '../albums/IRI/Photos/10.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/10.jpg'
+          },{
+              src: '../albums/IRI/Photos/11.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/11.jpg'
+          },{
+              src: '../albums/IRI/Photos/12.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/12.jpg',
+          },{
+              src: '../albums/IRI/Photos/13.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/13.jpg'
+          },{
+              src: '../albums/IRI/Photos/14.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/14.jpg'
+          },{
+              src: '../albums/IRI/Photos/15.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/15.jpg'
+          },{
+              src: '../albums/IRI/Photos/16.jpg',
+              thumb: '../albums/IRI/Photos/thumbs/16.jpg'
+          }]
+      });
+  });
+});
+
+
+$(document).ready(function() {
+  $('#seattle2013').on('click', function(e) {
+      $(this).lightGallery({
+          dynamic: true,
+          dynamicEl: [{
+              src: '../albums/Seattle2013/Photos/1.jpg',
+              thumb: '../albums/Seattle2013/Photos/thumbs/1.jpg'
+          },{
+              src: '../albums/Seattle2013/Photos/2.jpg',
+              thumb: '../albums/Seattle2013/Photos/thumbs/2.jpg',
+          },{
+              src: '../albums/Seattle2013/Photos/3.jpg',
+              thumb: '../albums/Seattle2013/Photos/thumbs/3.jpg'
+          }]
+      });
+  });
+});
