502ee6255b07d8e701d683d35504925bd7575eac
John Brandt <johnb0@outlook.com>
2018-01-30 09:57:32 -0700

    delete the old stuff

--- albums/compile.sh
+++
@@ -1,8 +0,0 @@
-echo "going to $1thumbs"
-
-mkdir "$1thumbs"
-mogrify -path "$1thumbs" -resize 1080x1080 $1*.JPG
-mogrify -path "$1thumbs" -resize 1080x1080 $1*.jpg
-mogrify -path "$1thumbs" -resize 1080x1080 $1*.PNG
-mogrify -path "$1thumbs" -resize 1080x1080 $1*.png
-echo "done"
--- contact.md
+++
@@ -1,8 +0,0 @@
----
-title: Contact Us
-layout: default
----
-
-For all inquiries related to the team, please email us! You can send us a
-message through [Facebook](https://www.facebook.com/Team4334/), or email a
-[mentor](mailto:joel@4334.ca).
